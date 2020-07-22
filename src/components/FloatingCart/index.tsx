63286ae2803014eafc7e4cd9fde1612e58874485
Lucas Cunha <lucasviana112@gmail.com>
2020-07-21 22:13:34 -0300

    implemantion of funcionality: calculate total price and total quantity of products

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -15,6 +15,14 @@ import formatValue from '../../utils/formatValue';
 
 import { useCart } from '../../hooks/cart';
 
+interface Product {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
+  quantity: number;
+}
+
 // Calculo do total
 // Navegação no clique do TouchableHighlight
 
@@ -25,14 +33,22 @@ const FloatingCart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce(
+      (accumulator, product) => accumulator + product.price * product.quantity,
+      0,
+    );
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce(
+      (accumulator, product) => accumulator + product.quantity,
+      0,
+    );
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
